5d1130a6a02ccf0326646c8ffb8642a3944c9d19
Sanvi Karthik <sanvikarthik11@gmail.com>
2024-06-21 07:39:56 +0530

    timegan hopefully

--- timegan.py
+++ timegan.py
@@ -19,7 +19,8 @@ Note: Use original data as training set to generater synthetic data (time-series
 # Necessary Packages
 import tensorflow as tf
 import numpy as np
-from utils import extract_time, rnn_cell, random_generator, batch_generator
+from TimeGAN.utils import extract_time, rnn_cell, random_generator, batch_generator
+
 
 
 def timegan (ori_data, parameters):
